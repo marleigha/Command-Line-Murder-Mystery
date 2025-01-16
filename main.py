2854058a2ac4f4206fc8861af73b41b56c242470
Marleigh Ausbrooks <ma108@wellesley.edu>
2025-01-16 14:32:44 -0500

    mystery.py is fixed but has no rolls

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys,time,random
 
-typing_speed = 101 #wpm
+typing_speed = 300 #wpm
 def slow_type(t):
     for l in t:
         sys.stdout.write(l)
@@ -12,6 +12,11 @@ def slow_type(t):
 # playerDexMod = 5
 # playerWisdomMod = -3
 # playerIntelligenceMod = 0
+
+# 1 == strength
+#2 == dexterity
+
+#add to the choice class a check type
 
 
 class Choice:
@@ -71,13 +76,13 @@ class Choice:
           
         self.succNextChoices[playerChoice].runChoice()
      
-c4 = Choice(["you go home and eat with your family"], [], [], [], [], False, 0)
-c5 = Choice(["you take a nap and the killer gets you"], [], [], [], [], False, 0)
-c6 = Choice(["the cat becomes your friend! yippee"], [], [], [], [], False, 0)
-c3 = Choice(["you look elsewhere and find a cat"], ["pet cat"], [], [c6], [], False, 0)
-c7 = Choice(["you make the door worse and the killer gets you"], [], [], [], [], False, 0)
-c2 = Choice(["you break the door down! you're safe", "hit the gym, the door didn't budge."], ["go home", "take a nap"], ["try to break the door again", "look elsewhere"], [c4, c5], [c7, c3], True, 19)
-c1 = Choice(["the door is locked"], ["break the door (you have to roll!)", "look elsewhere"], [], [c2, c3], [], False, 0)
+# c4 = Choice(["you go home and eat with your family"], [], [], [], [], False, 0)
+# c5 = Choice(["you take a nap and the killer gets you"], [], [], [], [], False, 0)
+# c6 = Choice(["the cat becomes your friend! yippee"], [], [], [], [], False, 0)
+# c3 = Choice(["you look elsewhere and find a cat"], ["pet cat"], [], [c6], [], False, 0)
+# c7 = Choice(["you make the door worse and the killer gets you"], [], [], [], [], False, 0)
+# c2 = Choice(["you break the door down! you're safe", "hit the gym, the door didn't budge."], ["go home", "take a nap"], ["try to break the door again", "look elsewhere"], [c4, c5], [c7, c3], True, 19)
+# c1 = Choice(["the door is locked"], ["break the door (you have to roll!)", "look elsewhere"], [], [c2, c3], [], False, 0)
      
-c1.runChoice()
+# c1.runChoice()
   
