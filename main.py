b2b0d23cf7cda03f429935d96720fdf323641314
Emily <ez107@wellesley.edu>
2025-01-16 15:09:06 -0500

    added modifier array example

--- main.py
+++ main.py
@@ -111,14 +111,14 @@ class Player:
 runner = Player("the runner", "won three highschool track medals", 20, 20, 10, 7, 12, 15)
 print(runner)
 
-     
+arr = ['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma']
 c4 = Choice(["you go home and eat with your family"], [], [], [], [], False, 0, "")
 c5 = Choice(["you take a nap and the killer gets you"], [], [], [], [], False, 0, "")
 c6 = Choice(["the cat becomes your friend! yippee"], [], [], [], [], False, 0, "")
 c3 = Choice(["you look elsewhere and find a cat"], ["pet cat"], [], [c6], [], False, 0, "")
 c7 = Choice(["you make the door worse and the killer gets you"], [], [], [], [], False, 0, "")
-c2 = Choice(["you break the door down! you're safe", "hit the gym, the door didn't budge."], ["go home", "take a nap"], ["try to break the door again", "look elsewhere"], [c4, c5], [c7, c3], True, 19, "charisma")
-c1 = Choice(["the door is locked"], ["break the door (you have to roll!)", "look elsewhere"], [], [c2, c3], [], False, 0, "constitution")
+c2 = Choice(["you break the door down! you're safe", "hit the gym, the door didn't budge."], ["go home", "take a nap"], ["try to break the door again", "look elsewhere"], [c4, c5], [c7, c3], True, 19, arr[0])
+c1 = Choice(["the door is locked"], ["break the door (you have to roll!)", "look elsewhere"], [], [c2, c3], [], False, 0, arr[3])
 
 c1.player_stats(runner)
 c1.runChoice()
